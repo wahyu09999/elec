8f737a41a6a48a2c526b909473953bc301e1f569
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-12-14 13:30:16 +0700

    dffd

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,10 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/mysql', function () {
-    Artisan::call('migrate:fresh --seed');
-  });
+    Artisan::call('migrate:rollback', ['--force' => true]);
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]);
+});
 
 Route::get('/', function () {
     return view('guest/guest');
