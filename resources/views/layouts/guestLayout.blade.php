c7d274bdcf8d7c290132d4ebb25d8b40fb32262f
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-12-14 14:32:50 +0700

    666

--- resources/views/layouts/guestLayout.blade.php
+++ resources/views/layouts/guestLayout.blade.php
@@ -11,23 +11,23 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('frontend/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('frontend/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/img/favicon.png" rel="icon">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('frontend/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/aos/aos.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet">
+  <link href="https://elec-inkkusd3za-uc.a.run.app/frontend/css/style.css" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Arsha - v4.7.1
@@ -44,15 +44,15 @@
     </main>
 
     <!-- Vendor JS Files -->
-  <script src="{{ asset('frontend/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/waypoints/noframework.waypoints.js') }}"></script>
-  <script src="{{ asset('frontend/vendor/php-email-form/validate.js') }}"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/aos/aos.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('frontend/js/main.js') }}"></script>
+  <script src="https://elec-inkkusd3za-uc.a.run.app/frontend/js/main.js"></script>
 </body>
 </html>
