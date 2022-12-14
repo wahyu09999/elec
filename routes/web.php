2273cf1416ef65686bd221598474d86954a6e2ce
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-12-14 13:38:22 +0700

    dfdfdfdf

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/mysql', function () {
-    // Artisan::call('migrate:rollback', ['--force' => true]);
+    Artisan::call('migrate:rollback', ['--force' => true]);
     Artisan::call('migrate', ['--force' => true]);
     Artisan::call('db:seed', ['--force'=>true]);
 });
